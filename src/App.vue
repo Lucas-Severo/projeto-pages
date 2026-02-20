39b366a1b9a68835a75a2b0d071eab887ebac2a4
Lucas Rodrigues Severo <lucasrodriguesevero@gmail.com>
2026-02-20 13:21:08 -0400

    Alterado arquivo actions

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About changed</RouterLink>
+        <RouterLink to="/:pathMatch(.*)*">NotFound</RouterLink>
       </nav>
     </div>
   </header>
